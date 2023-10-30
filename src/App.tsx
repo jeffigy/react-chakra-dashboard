018bb91fc19d60d73b65869390738395f82d13dd
jeffigy <ejeffydev@gmail.com>
2023-10-30 21:12:01 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Button } from "@chakra-ui/react";
+import React from "react";
+
+type AppProps = {};
+
+const App: React.FC<AppProps> = () => {
+  return (
+    <Box m={2}>
+      <Button>hehe</Button>
+    </Box>
+  );
+};
+export default App;
---
+++ src/chakra/theme.ts
@@ -0,0 +1,26 @@
+import { extendTheme } from "@chakra-ui/react";
+import { Button } from "./Button";
+
+const colors = {
+  brand: {
+    50: "#DAF6FF",
+    100: "#ADDEFF",
+    200: "#7CC7FF",
+    300: "#4AB1FF",
+    400: "#1A9AFF",
+    500: "#0081E6",
+    600: "#0064B4",
+    700: "#004782",
+    800: "#002B51",
+    900: "#000F21",
+    bg: "#F5F6FA",
+  },
+};
+
+const theme = extendTheme({
+  colors,
+  components: {
+    Button: Button,
+  },
+});
+export default theme;
